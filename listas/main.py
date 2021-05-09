8a31bea68985bc76ecbc8078d59c9ebeb65d0873
doug <d.bianchezzi@gmail.com>
2021-05-09 09:32:28 -0300

    subindo repositório

---
+++ listas/main.py
@@ -0,0 +1,2 @@
+# criar algumas colecoes com valores diversos para representar um pouco a flexibilidade
+# das listas em python.
---
+++ strings/main.py
@@ -0,0 +1,8 @@
+print("Olá mundo!")
+
+primeiro_nome = "Douglas"
+segundo_nome = "Bianchezzi"
+
+print(primeiro_nome + " " + segundo_nome)
+
+print(f"Olá, {primeiro_nome} {segundo_nome}, seja bem vind(a-o)")
